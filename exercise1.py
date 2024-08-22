eb1636973f49c8440c9c2c37bf88877823b36f48
Mohadeseh Zarei <mohadesehzarei319@gmail.com>
2024-08-22 22:30:22 +0330

    Delete exercise1.py

--- exercise1.py
+++
@@ -1,9 +0,0 @@
-score1=float(input("enter your score1:"))
-if score1>=12:
-    print("you are passed")
-else :print("you are failled")
-
-score2=float(input("enter your score2:"))
-if score2>=12:
-    print("you are passed")
-else :print("you are faild")
